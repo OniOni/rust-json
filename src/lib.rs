94d05999a70e7bac5b0642fe85003cc4028919d2
OniOni <mathieu.c.sabourin@gmail.com>
2015-05-31 15:18:09 -0700

    Build Json object.

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,14 @@ pub mod parser {
         String(string::String),
         Object(self::Object),
         Array(self::Array),
+        U64(u64),
         Null
     }
 
     pub type Array = Vec<Json>;
     pub type Object = BTreeMap<string::String, Json>;
 
-    fn parse_value(it: &mut Chars) {
+    fn parse_value(it: &mut Chars) -> Json {
         match it.next() {
             Some(' ') => parse_value(it),
             Some('{') => parse_object(it),
@@ -28,42 +29,43 @@ pub mod parser {
                     for car in it.take_while(|a| a.is_digit(10)) {
                         num_str.push(car);
                     }
-                    let num = num_str.parse::<u32>().unwrap();
+                    Json::U64(num_str.parse::<u64>().unwrap())
+                } else {
+                    Json::Null
                 }
             },
-            None => println!("Error occured")
+            None => Json::Null
         }
     }
 
-    fn parse_object(it: &mut Chars) {
+    fn parse_object(it: &mut Chars) -> Json {
         let mut collector = "".to_string();
+        let mut object = BTreeMap::new();
         loop {
             match it.next() {
                 Some(':') => {
-                    println!("Key is ready: {}", collector);
+                    object.insert(collector, parse_value(it));
                     collector = "".to_string();
-                    parse_value(it);
                 },
-                Some(',') => collector = "".to_string(),
+                Some(',') => {collector = "".to_string()},
                 Some('}') => break,
                 Some(c) => {
                     collector.push(c);
-                    println!("Collecting: {}", collector);
                 },
                 None => break
             }
         }
+        Json::Object(object)
     }
 
-    pub fn parse(json: &str) {
-        let finished = false;
+    pub fn parse(json: &str) -> Json {
         let mut it = json.chars();
 
-        while !finished {
+        loop {
             match it.next() {
-                Some('{') => parse_object(&mut it),
-                _ => break
-            };
+                Some('{') => return parse_object(&mut it),
+                _ => return Json::Null
+            }
         }
     }
 }
