d64d47be50994bcee1efd5055bfbd38adc23774b
OniOni <mathieu.c.sabourin@gmail.com>
2015-06-02 22:21:26 -0700

    Parse a sligtly more complex object.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate json;
 
 fn main() {
-    let json = json::parser::parse("{'key': 42}");
+    for _ in 0..1000 {
+        json::parser::parse("{\"key\": 42, \"key2\": {\"key3\": 1}}");
+    }
+
+    let json = json::parser::parse("{\"key\": 42, \"key2\": {\"key3\": 1}}");
     println!("{:?}", json);
 }
