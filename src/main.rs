d36340caf0f3d24a8e71c636de28a2da0b51951a
OniOni <mathieu.c.sabourin@gmail.com>
2015-06-05 19:39:14 -0700

    Add example with string.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ fn main() {
         json::parser::parse("{\"key\": 42, \"key2\": {\"key3\": 1}}");
     }
 
-    let json = json::parser::parse("{\"key\": 42, \"key2\": {\"key3\": 1}}");
+    let json = json::parser::parse("{\"key\": 42, \"key2\": {\"key3\": \"lol\"}}");
     println!("{:?}", json);
 }
